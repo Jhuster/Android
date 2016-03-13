c77b032577f0dd025a64c1f60d8cc786c477bf15
jhuster <lujun.hust@gmail.com>
2016-03-13 18:13:02 +0800

    tab to space

--- Audio/AudioPlayer.java
+++ Audio/AudioPlayer.java
@@ -32,18 +32,18 @@ public class AudioPlayer {
     private int mMinBufferSize = 0;
     private AudioTrack mAudioTrack;  
     
-	public boolean startPlayer() {
-	    return startPlayer(DEFAULT_STREAM_TYPE,DEFAULT_SAMPLE_RATE,DEFAULT_CHANNEL_CONFIG,DEFAULT_AUDIO_FORMAT);
-	}
-	
-	public boolean startPlayer(int streamType, int sampleRateInHz, int channelConfig, int audioFormat) {
-	    
-	    if (mIsPlayStarted) {
-	        Log.e(TAG, "Player already started !");
-	        return false;
-	    }
-	    
-	    mMinBufferSize = AudioTrack.getMinBufferSize(sampleRateInHz,channelConfig,audioFormat);
+    public boolean startPlayer() {
+        return startPlayer(DEFAULT_STREAM_TYPE,DEFAULT_SAMPLE_RATE,DEFAULT_CHANNEL_CONFIG,DEFAULT_AUDIO_FORMAT);
+    }
+    
+    public boolean startPlayer(int streamType, int sampleRateInHz, int channelConfig, int audioFormat) {
+        
+        if (mIsPlayStarted) {
+            Log.e(TAG, "Player already started !");
+            return false;
+        }
+        
+        mMinBufferSize = AudioTrack.getMinBufferSize(sampleRateInHz,channelConfig,audioFormat);
         if (mMinBufferSize == AudioTrack.ERROR_BAD_VALUE) {
             Log.e(TAG, "Invalid parameter !");
             return false;
@@ -54,21 +54,21 @@ public class AudioPlayer {
         if (mAudioTrack.getState() == AudioTrack.STATE_UNINITIALIZED) {
             Log.e(TAG, "AudioTrack initialize fail !");
             return false;
-        }   	     
+        }            
         
         mIsPlayStarted = true;
         
         Log.d(TAG, "Start audio player success !");
         
-	    return true;
-	}
-	
-	public int getMinBufferSize() {
-	    return mMinBufferSize;
-	}
-	
-	public void stopPlayer() {
-	    
+        return true;
+    }
+    
+    public int getMinBufferSize() {
+        return mMinBufferSize;
+    }
+    
+    public void stopPlayer() {
+        
         if (!mIsPlayStarted) {
             return;
         }
@@ -79,30 +79,30 @@ public class AudioPlayer {
         
         mAudioTrack.release();
         mIsPlayStarted = false;
-	       
-	    Log.d(TAG, "Stop audio player success !");
-	}
-	
-	public boolean play(byte[] audioData, int offsetInBytes, int sizeInBytes) {
-	    
-	    if (!mIsPlayStarted) {
-	        Log.e(TAG, "Player not started !");
-	        return false;
-	    }
-	    
-	    if (sizeInBytes < mMinBufferSize) {
-	        Log.e(TAG, "audio data is not enough !");
-	        return false;
-	    }
-	    
-	    if (mAudioTrack.write(audioData,offsetInBytes,sizeInBytes) != sizeInBytes) {                
-	        Log.e(TAG, "Could not write all the samples to the audio device !");
+           
+        Log.d(TAG, "Stop audio player success !");
+    }
+    
+    public boolean play(byte[] audioData, int offsetInBytes, int sizeInBytes) {
+        
+        if (!mIsPlayStarted) {
+            Log.e(TAG, "Player not started !");
+            return false;
+        }
+        
+        if (sizeInBytes < mMinBufferSize) {
+            Log.e(TAG, "audio data is not enough !");
+            return false;
+        }
+        
+        if (mAudioTrack.write(audioData,offsetInBytes,sizeInBytes) != sizeInBytes) {                
+            Log.e(TAG, "Could not write all the samples to the audio device !");
         }                                   
-                                    	    	   
+                                                   
         mAudioTrack.play();
         
         Log.d(TAG , "OK, Played "+sizeInBytes+" bytes !");
         
         return true;
-	}
+    }
 }
